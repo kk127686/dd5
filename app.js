b27eb532a97f300edafdf6b3ec9e5743b1a7141e
kk127686 <46560445+kk127686@users.noreply.github.com>
2023-09-27 10:15:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express=require("express");
 const mysql=require("mysql");
 var app=express();//连接数据库
-var db=mysql.createConnection({host: process.env.MYSQL_HOST,
+const mysqlOptions={host: process.env.MYSQL_HOST,
 port: process.env.MYSQL_PORT,
 user: process.env.MYSQL_USER,
 password: process.env.MYSQL_PASSWORD,
 database: process.env.MYSQL_DATABASE
-});
-
+}
+var db=mysql.createConnection(mysqlOptions);
 app.get("/",function(req,res){
-	//2.发送请求(查询)
+	res.send({"code":200,"msg":"Hello,world"});
+});
+app.get("/list",function(req,res){
 	db.query("SELECT * FROM students",function(err,data){
 		if(err){
 			console.log("数据库访问出错",err);
-            res.send(err)
+	    res.send(err)
 		}else{
 			console.log(data);
-            res.send(data);
+	    res.send(data);
 		}
 	})
 	res.send("express");
+});
+app.get("/info",function(req,res){
+	res.send(mysqlOptions);
 });
 app.listen(8080);
